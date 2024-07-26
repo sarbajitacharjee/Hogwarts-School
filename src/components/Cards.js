2fbe1ac1c0f0a8e6654ef98f3b8c5b5aaa586333
Sarbajit Acharjee <sarbajitacharjee712@gmail.com>
2024-07-26 11:13:01 +0530

    simple add photos

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import mypicture from '../assets/mypicture.jpg'
 
 const Cards = () => {
   return (
     <>
 
 <div className="profile bg-white p-6  rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-          <img  src='./images/mypicture.jpg' alt='sarbajit' className='h-[400px] w-[450px] object-contain rounded-full'/>
+          <img  src={mypicture} alt='sarbajit' className='h-[400px] w-[450px] object-contain rounded-full'/>
           <h3 className="text-xl font-semibold mb-2">Er. Sarbajit Acharjee</h3>
           <p className="text-gray-700 mb-4">Frontend Engineer </p>
           <button className="text-white bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg">Read More</button>
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,47 +2,56 @@
 import React from 'react';
 import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-
+import schoolevent1 from '../assets/schoolevent1.jpg'
+import schoolevent2 from '../assets/schoolevent2.jpg'
+import schoolevent3 from '../assets/schoolevent3.jpg'
+import schoolevent4 from '../assets/schoolevent4.jpg'
+import schoolevent5 from '../assets/schoolevent5.jpg'
+import schoolevent6 from '../assets/schoolevent6.jpg'
+import schoolevent7 from '../assets/schoolevent7.jpg'
+import schoolevent8 from '../assets/schoolevent8.jpg'
+import schoolevent9 from '../assets/schoolevent9.jpg'
+import schoolevent10 from '../assets/schoolevent10.jpg'
 const Carousel = () => (
   <ResponsiveCarousel showArrows autoPlay infiniteLoop>
     <div className="h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] w-full">
-      <img src="./images/schoolevent1.jpg" alt="Event 1" className="h-full w-full object-contain" />
+      <img src={schoolevent1} alt="Event 1" className="h-full w-full object-contain" />
       <p className="legend bg-gray-800    md:block text-white p-2 rounded">Railway Station to Hogwarts</p>
     </div>
     <div className="h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] w-full">
-      <img src="./images/schoolevent2.jpg" alt="Event 2" className="h-full w-full object-contain" />
+      <img src={schoolevent2} alt="Event 2" className="h-full w-full object-contain" />
       <p className="legend bg-gray-800    md:block text-white p-2 rounded">Train To Hogwarts</p>
     </div>
     <div className="h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] w-full">
-      <img src="./images/schoolevent3.jpg" alt="Event 3" className="h-full w-full object-contain" />
+      <img src={schoolevent3} alt="Event 3" className="h-full w-full object-contain" />
       <p className="legend bg-gray-800    md:block text-white p-2 rounded">Hogwarts Castle</p>
     </div>
     <div className="h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] w-full">
-      <img src="./images/schoolevent4.jpg" alt="Event 4" className="h-full w-full object-contain" />
+      <img src={schoolevent4} alt="Event 4" className="h-full w-full object-contain" />
       <p className="legend bg-gray-800    md:block text-white p-2 rounded">Magic Room</p>
     </div>
     <div className="h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] w-full">
-      <img src="./images/schoolevent5.jpg" alt="Event 5" className="h-full w-full object-contain" />
+      <img src={schoolevent5} alt="Event 5" className="h-full w-full object-contain" />
       <p className="legend bg-gray-800    md:block text-white p-2 rounded">Stationary Shop</p>
     </div>
     <div className="h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] w-full">
-      <img src="./images/schoolevent6.jpg" alt="Event 6" className="h-full w-full object-contain" />
+      <img src={schoolevent6} alt="Event 6" className="h-full w-full object-contain" />
       <p className="legend bg-gray-800    md:block text-white p-2 rounded">Welcome Letters</p>
     </div>
     <div className="h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] w-full">
-      <img src="./images/schoolevent7.jpg" alt="Event 7" className="h-full w-full object-contain" />
+      <img src={schoolevent7} alt="Event 7" className="h-full w-full object-contain" />
       <p className="legend bg-gray-800    md:block text-white p-2 rounded">Way TO Hogwarts</p>
     </div>
     <div className="h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] w-full">
-      <img src="./images/schoolevent8.jpg" alt="Event 8" className="h-full w-full object-contain" />
+      <img src={schoolevent8} alt="Event 8" className="h-full w-full object-contain" />
       <p className="legend bg-gray-800    md:block text-white p-2 rounded">Big Watch tower</p>
     </div>
     <div className="h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] w-full">
-      <img src="./images/schoolevent9.jpg" alt="Event 9" className="h-full w-full object-contain" />
+      <img src={schoolevent9} alt="Event 9" className="h-full w-full object-contain" />
       <p className="legend bg-gray-800    md:block text-white p-2 rounded">Uniform</p>
     </div>
     <div className="h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] w-full">
-      <img src="./images/schoolevent10.jpg" alt="Event 10" className="h-full w-full object-contain" />
+      <img src={schoolevent10} alt="Event 10" className="h-full w-full object-contain" />
       <p className="legend bg-gray-800    md:block text-white p-2 rounded">Dress For different Catagories</p>
     </div>
     
